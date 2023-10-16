d08c4fc50bfdce8eb68e206395e878935d2d9bf8
drsky <contact@drsky.ovh>
2023-10-16 15:30:42 +0200

    Integrate i18next provider and import App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './config/i18n.js';
+
 import './index.css';
+import './styles/reset.css';
+import './styles/variables.css';
+import './styles/global.module.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
