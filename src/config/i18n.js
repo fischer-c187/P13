4e903fb4a48cd28ff6b5be42c83d7b95ae568d2f
drsky <contact@drsky.ovh>
2023-10-16 14:52:44 +0200

    update name navigation to applayout

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,11 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import navigationEN from '../locales/en/navigation.json';
+import appLayoutEN from '../locales/en/appLayout.json';
 
 const resources = {
   en: {
-    navigation: navigationEN,
+    appLayout: appLayoutEN,
   },
 };
 
