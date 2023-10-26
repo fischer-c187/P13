b2482b88e58b92fd969e78ea5e183465bd83c89a
drsky <contact@drsky.ovh>
2023-10-26 15:38:16 +0200

    Implement user selector for Redux state management

--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,12 +1,16 @@
+import { useSelector } from 'react-redux';
 import UserPresentation from '../../components/userPresentation/userPresentation';
 import Transactions from '../../layouts/transactions/transactions';
 
 import styles from './user.module.css';
+import { selectUser } from '../../store/slice/authSlice';
 
 function User() {
+  const user = useSelector(selectUser);
+
   return (
     <main className={styles.main}>
-      <UserPresentation firstname='mm' lastname='tt' />
+      <UserPresentation firstname={user.firstName} lastname={user.lastName}  />
       <Transactions />
     </main>
   );
