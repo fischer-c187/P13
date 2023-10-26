0a7afd7d7a3034c36344b0268ef33a6186d29c30
drsky <contact@drsky.ovh>
2023-10-26 15:04:17 +0200

    Implement error handling for failed queries using RTK Query

--- src/components/editUserForm/editUserForm.jsx
+++ src/components/editUserForm/editUserForm.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 
 import InputField from '../inputField/inputField';
+import ErrorForm from '../errorForm/errorForm';
+import { useUpdateUserMutation } from '../../store/slice/apiSlice';
+import { setUserInfo } from '../../store/slice/authSlice';
 
 import styles from './editUserForm.module.css';
-import { useTranslation } from 'react-i18next';
 
-function EditUserForm({ firstname, lastname, handleClickCancel }) {
+/**
+ * EditUserForm - A form component for editing a user's first and last name.
+ *
+ * This component provides input fields for editing the user's first and last name.
+ * It also handles form submission and cancellation.
+ * Upon successful update, it dispatches the new user info to the Redux store and triggers a cancellation function.
+ *
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {string} props.firstName - The current first name of the user.
+ * @param {string} props.lastName - The current last name of the user.
+ * @param {Function} props.handleClickCancel - Function to handle the cancellation of the edit.
+ *
+ */
+function EditUserForm({ firstName, lastName, handleClickCancel }) {
   const { t } = useTranslation('userPage');
+  const [updateUser, updateResult] = useUpdateUserMutation();
+  const dispatch = useDispatch();
   const [user, setUser] = useState({
-    firstname: firstname,
-    lastname: lastname,
+    firstName: firstName,
+    lastName: lastName,
   });
+
+  useEffect(() => {
+    if (updateResult.isSuccess) {
+      dispatch(setUserInfo(user));
+      handleClickCancel();
+    }
+  }, [updateResult.isSuccess]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -21,25 +48,43 @@ function EditUserForm({ firstname, lastname, handleClickCancel }) {
     }));
   };
 
+  const handleSubmitEditUser = (event) => {
+    event.preventDefault();
+    if (user.firstName !== firstName || user.lastName !== lastName) {
+      updateUser(user);
+    } else {
+      handleClickCancel();
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmitEditUser}>
+      {updateResult.isError && (
+        <div className={styles.errorWrapper}>
+          <ErrorForm errorText={`errors.${updateResult.error.errorKey}`} />
+        </div>
+      )}
       <div className={styles.inputContainer}>
         <InputField
-          name='firstname'
-          placeholder={firstname}
-          value={user.firstname}
+          name='firstName'
+          placeholder={firstName}
+          value={user.firstName}
           handleChange={handleChange}
         />
         <InputField
-          name='lastname'
-          placeholder={lastname}
-          value={user.lastname}
+          name='lastName'
+          placeholder={lastName}
+          value={user.lastName}
           handleChange={handleChange}
         />
       </div>
 
       <div className={styles.btnContainer}>
-        <button type='submit' className={styles.btn}>
+        <button
+          type='submit'
+          className={styles.btn}
+          disabled={updateResult.isLoading}
+        >
           {t('saveBtn')}
         </button>
         <button
--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -1,11 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import InputField from '../inputField/inputField';
 import CheckboxField from '../checkboxField/checkboxField';
+import { useLazyGetTokenQuery } from '../../store/slice/apiSlice';
+import { setTokenInStorage } from '../../utils/token';
+import { login, selectIsAuth } from '../../store/slice/authSlice';
+import ErrorForm from '../errorForm/errorForm';
 
 import styles from './loginForm.module.css';
+
 
 const initialState = {
   username: '',
@@ -13,9 +20,39 @@ const initialState = {
   remember: false,
 };
 
+/**
+ * Provides a form for user authentication, including fields for username, password, and an optional "remember me" checkbox.
+ * 
+ * 
+ * This component handles user login by capturing the username and password,
+ * invoking a query to get a token, and then dispatching actions to update the Redux state.
+ * The component also makes use of various hooks for state management and side-effects.
+ * It employs i18n for internationalization and utilizes FontAwesome icons for improved UX.
+ * 
+ * Upon successful login, it navigates to the user's homepage. 
+ * In case of an error, an error message is displayed.
+ * 
+ */
 function LoginForm() {
   const [user, setUser] = useState(initialState);
   const { t } = useTranslation('loginPage');
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+  const [getToken, getTokenResult] = useLazyGetTokenQuery();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (getTokenResult.isSuccess) {
+      dispatch(login());
+      setTokenInStorage(getTokenResult.data, user.remember);
+    }
+  }, [getTokenResult.isSuccess, getTokenResult.data, user.remember, dispatch]);
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate('/user');
+    }
+  }, [isAuth, navigate]);
 
   const handleChange = (event) => {
     const { name, type, checked, value } = event.target;
@@ -26,11 +63,22 @@ function LoginForm() {
     }));
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getToken({
+      email: user.username,
+      password: user.password,
+    });
+  };
+
   return (
     <section className={styles.containerForm}>
       <FontAwesomeIcon className={styles.icon} icon='fa-solid fa-circle-user' />
       <h1 className={styles.title}>{t('title')}</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        {getTokenResult.isError && (
+          <ErrorForm errorText={t(`errors.${getTokenResult.error.errorKey}`)} />
+        )}
         <InputField
           name='username'
           label={t('labelUsername')}
@@ -43,6 +91,7 @@ function LoginForm() {
           label={t('labelPassword')}
           handleChange={handleChange}
           value={user.password}
+          type='password'
         />
 
         <CheckboxField
@@ -52,8 +101,8 @@ function LoginForm() {
           checked={user.remember}
         />
 
-        <button className={styles.btn} type='submit'>
-          {t('buttonText')}
+        <button className={styles.btn} type='submit' disabled={getTokenResult.isFetching}>
+          {getTokenResult.isFetching ? 'loading...' : t('buttonText')}
         </button>
       </form>
     </section>
