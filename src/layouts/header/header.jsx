563c0c6e9283dda60554f1e1ee510ad55b8b57cd
drsky <contact@drsky.ovh>
2023-10-26 15:34:53 +0200

    Implement authenticated user navigation flow

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -1,24 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate  } from 'react-router-dom';
 
-import { navigationUserConnected, navigationUserNotConnected } from '../../constants/navigation';
+import {
+  navigationUserConnected,
+  navigationUserNotConnected,
+} from '../../constants/navigation';
 import Navigation from '../../components/navigation/navigation';
 import generalAppInformation from '../../constants/general';
 
 import styles from './header.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsAuth, selectUser } from '../../store/slice/authSlice';
 
 /**
  * The `Header` component renders the header of the application.
  * It includes a logo and navigation links.
- * 
+ *
  * @returns {JSX.Element} A JSX element representing the header.
  */
 function Header() {
-  const items = navigationUserNotConnected;
+  const isAuth = useSelector(selectIsAuth);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const items = isAuth
+    ? navigationUserConnected(user.firstName, dispatch, navigate)
+    : navigationUserNotConnected;
 
   return (
     <header className={styles.header}>
       <Link to='/'>
-        <img src={generalAppInformation.logo} className={styles.header__logo} alt="logo click to return to the home page" />
+        <img
+          src={generalAppInformation.logo}
+          className={styles.header__logo}
+          alt='logo click to return to the home page'
+        />
         <h1 className='sr'>Argent Bank</h1>
       </Link>
       <Navigation navigationItems={items} />
