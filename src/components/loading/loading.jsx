a3adb430d614e100dce81154c70a8479d15f7f4a
drsky <contact@drsky.ovh>
2023-10-26 15:10:44 +0200

    Add loading components for initial app startup

---
+++ src/components/loading/loading.jsx
@@ -0,0 +1,21 @@
+import styles from './loading.module.css';
+
+function Loading() {
+  return (
+    <div className={styles.ldsGrid}>
+      <div >
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
